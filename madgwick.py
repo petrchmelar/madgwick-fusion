44058ce9465336e35219e79fbc9461ce74012bef
petrchmelar179 <petrchmelar179@gmail.com>
2017-10-07 11:02:49 +0200

    Add objective function claculation

--- madgwick.py
+++ madgwick.py
@@ -13,7 +13,7 @@ class MadgwickFusion:
     last_gyro = List[float]
     last_magneto = List[float]
 
-    estimated_orientation = np.quaternion(0, 0, 0, 0)
+    SE_q = np.quaternion(0, 0, 0, 0)
     b_x = 0.0 # reference direction of flux in earth frame
     b_z = 0.0 # reference direction of flux in earth frame
     est_gyro_bias = List[float]
@@ -31,6 +31,16 @@ class MadgwickFusion:
         acc_norm = self.normalize_vector(acc)
         gyro_norm = self.normalize_vector(gyro)
         magneto = self.normalize_vector(magneto)
+
+        # compute the objective function and jacobian
+        SE_q_comp = self.SE_q.components
+        f = np.array(
+            [2 * SE_q_comp[0] * SE_q_comp[3] - 2 * SE_q_comp[0] * SE_q_comp[2] - acc[0],
+             2 * SE_q_comp[1] * SE_q_comp[1] + 2 * SE_q_comp[2] * SE_q_comp[3] - acc[1],
+             1 - 2 * SE_q_comp[1] * SE_q_comp[1] - 2 * SE_q_comp[2] * SE_q_comp[2] - acc[2],
+             2 * self.b_x * (0.5 - SE_q_comp[2] * SE_q_comp[2] - SE_q_comp[3] * SE_q_comp[3]) + 2 * self.b_z * (SE_q_comp[1] * SE_q_comp[3] - SE_q_comp[0] * SE_q_comp[2]) - magneto[0],
+             2 * self.b_x * (SE_q_comp[1] * SE_q_comp[2] - SE_q_comp[0] * SE_q_comp[3] + 2 * self.b_z * (SE_q_comp[0] * SE_q_comp[1] + SE_q_comp[2] * SE_q_comp[3]) - magneto[1]),
+             2 * self.b_x * (SE_q_comp[0] * SE_q_comp[3]) + 2 * self.b_z * (0.5 - SE_q_comp[1] * SE_q_comp[1] - SE_q_comp[2] * SE_q_comp[2]) - magneto[2]])
 
     def normalize_vector(self, vector):
         norm = np.linalg.norm(vector, ord=2)
