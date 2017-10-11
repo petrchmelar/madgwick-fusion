796ad964a723518ff0f7395d723fe98b1efec940
petrchmelar179 <petrchmelar179@gmail.com>
2017-10-11 19:36:56 +0200

    Add methods representing objective functions and its jacobians

--- madgwick.py
+++ madgwick.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
-import quaternion
+from scipy import optimize
+from pyquaternion import Quaternion
+
 from typing import List
 
 class MadgwickFusion:
@@ -13,7 +15,7 @@ class MadgwickFusion:
     last_gyro = List[float]
     last_magneto = List[float]
 
-    SE_q = np.quaternion(0, 0, 0, 0)
+    SE_q = np.array([0, 0, 0, 0], np.float)
     b_x = 0.0 # reference direction of flux in earth frame
     b_z = 0.0 # reference direction of flux in earth frame
     est_gyro_bias = List[float]
@@ -28,20 +30,62 @@ class MadgwickFusion:
 
     def filter_update(self, acc, gyro, magneto):
         # normalize data
-        acc_norm = self.normalize_vector(acc)
-        gyro_norm = self.normalize_vector(gyro)
-        magneto_norm = self.normalize_vector(magneto)
+        acc_norm = np.insert(self.normalize_vector(acc), 0, 0)
+        gyro_norm = np.insert(self.normalize_vector(gyro), 0, 0)
+        magneto_norm = np.insert(self.normalize_vector(magneto), 0, 0)
 
-        # compute the objective function and jacobian
-        SE_q_comp = self.SE_q.components
-        f = np.array(
-            [2 * SE_q_comp[0] * SE_q_comp[3] - 2 * SE_q_comp[0] * SE_q_comp[2] - acc_norm[0],
-             2 * SE_q_comp[1] * SE_q_comp[1] + 2 * SE_q_comp[2] * SE_q_comp[3] - acc_norm[1],
-             1 - 2 * SE_q_comp[1] * SE_q_comp[1] - 2 * SE_q_comp[2] * SE_q_comp[2] - acc_norm[2],
-             2 * self.b_x * (0.5 - SE_q_comp[2] * SE_q_comp[2] - SE_q_comp[3] * SE_q_comp[3]) + 2 * self.b_z * (SE_q_comp[1] * SE_q_comp[3] - SE_q_comp[0] * SE_q_comp[2]) - magneto_norm[0],
-             2 * self.b_x * (SE_q_comp[1] * SE_q_comp[2] - SE_q_comp[0] * SE_q_comp[3] + 2 * self.b_z * (SE_q_comp[0] * SE_q_comp[1] + SE_q_comp[2] * SE_q_comp[3]) - magneto_norm[1]),
-             2 * self.b_x * (SE_q_comp[0] * SE_q_comp[3]) + 2 * self.b_z * (0.5 - SE_q_comp[1] * SE_q_comp[1] - SE_q_comp[2] * SE_q_comp[2]) - magneto_norm[2]])
+        # Group 1 magnetic distortion compensation recalculation
+        E_h = self.SE_q * np.transpose(magneto_norm) * np.transpose(Quaternion(self.SE_q).conjugate.elements)
+        E_b = np.array([0, np.linalg.norm(np.array([E_h[0], E_h[1]]), ord=2), 0, E_h[3]])
+
+        # magnetometer and accelerometer fusion
+
+        # # compute SE_q using (14)
+        # optimize.minimize(lambda SE_q, E_d, S_s: np.tensordot((np.tensordot(SE_q, E_d, axes=0)), S_s, axes=0), self.SE_q, )
+        #
+        # f = np.array(
+        #     [2 * self.SE_q[0] * self.SE_q[3] - 2 * self.SE_q[0] * self.SE_q[2] - acc_norm[0],
+        #      2 * self.SE_q[1] * self.SE_q[1] + 2 * self.SE_q[2] * self.SE_q[3] - acc_norm[1],
+        #      1 - 2 * self.SE_q[1] * self.SE_q[1] - 2 * self.SE_q[2] * self.SE_q[2] - acc_norm[2],
+        #      2 * self.b_x * (0.5 - self.SE_q[2] * self.SE_q[2] - self.SE_q[3] * self.SE_q[3]) + 2 * self.b_z * (self.SE_q[1] * self.SE_q[3] - self.SE_q[0] * self.SE_q[2]) - magneto_norm[0],
+        #      2 * self.b_x * (self.SE_q[1] * self.SE_q[2] - self.SE_q[0] * self.SE_q[3] + 2 * self.b_z * (self.SE_q[0] * self.SE_q[1] + self.SE_q[2] * self.SE_q[3]) - magneto_norm[1]),
+        #      2 * self.b_x * (self.SE_q[0] * self.SE_q[3]) + 2 * self.b_z * (0.5 - self.SE_q[1] * self.SE_q[1] - self.SE_q[2] * self.SE_q[2]) - magneto_norm[2]])
 
     def normalize_vector(self, vector):
         norm = np.linalg.norm(vector, ord=2)
         return [vector[0] / norm, vector[1] / norm, vector[2] / norm]
+
+    def J_g(self, SE_q):
+        return np.array([
+            [-2 * SE_q[2], 2 * SE_q[3], -2 * SE_q[0], 2 * SE_q[1]],
+            [2 * SE_q[1], 2 * SE_q[0], 2 * SE_q[3], 2 * SE_q[2]],
+            [0 -4 * SE_q[1], -4 * SE_q[2], 0]
+        ])
+
+    def J_b(self, SE_q, E_b):
+        return np.array([
+            [-2 * E_b[2] * SE_q[2], 2 * E_b[2] * SE_q[3], -4 * E_b[0] * SE_q[2] - 2 * E_b[2] * SE_q[0],
+             -4 * E_b[0] * SE_q[3] + 2 * E_b[2] * SE_q[1]],
+            [-2 * E_b[0] * SE_q[3] + 2 * E_b[2] * SE_q[1], 2 * E_b[0] * SE_q[2] + 2 * E_b[2] * SE_q[0],
+             2 * E_b[0] * SE_q[1] + 2 * E_b[2] * SE_q[3], -2 * E_b[0] * SE_q[0] + 2 * E_b[2] * SE_q[2]],
+            [2 * E_b[0] * SE_q[2], 2 * E_b[0] * SE_q[3] - 4 * E_b[2] * SE_q[1], 2 * E_b[0] * SE_q[0] -
+             4 * E_b[2] * SE_q[2], 2 * E_b[0] * SE_q[1]]
+        ])
+
+    def f_g(self, SE_q, S_a):
+        return np.array([
+            [2 * (SE_q[1] * SE_q[3] - SE_q[0] * SE_q[2]) - S_a[0]],
+            [2 * (SE_q[0] * SE_q[1] + SE_q[2] * SE_q[3]) - S_a[1]],
+            [2 * (0.5 - SE_q[1]**2 - SE_q[2]**2) - S_a[2]]
+        ])
+
+    def f_b(self, SE_q, E_b, S_m):
+        return np.array(
+            [[2 * E_b[0] * (0.5 - SE_q[2]**2 - SE_q[3]**2) + 2 * E_b[2] * (SE_q[1] * SE_q[3] - SE_q[0] * SE_q[2]) -
+              S_m[0]],
+             [2 * E_b[0] * (SE_q[1] * SE_q[2] - SE_q[0]*SE_q[3]) + 2 * E_b[2] * (SE_q[0] * SE_q[1] + SE_q[2] * SE_q[3]) -
+              S_m[1]],
+             [2 * E_b[0] * (SE_q[0] * SE_q[2] + SE_q[1] * SE_q[3]) + 2 * E_b[2] * (0.5 - SE_q[1]**2 - SE_q[2]**2)
+              - S_m[1]]])
+
+    def Jg
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import quaternion
 from madgwick import MadgwickFusion
 
 def main():
